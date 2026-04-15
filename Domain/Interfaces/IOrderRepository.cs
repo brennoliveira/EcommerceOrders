ed7e2da56627a13937a2a7df7b4dd7bc213555a6
brennoliveira <brenno.oliveira@souunit.com.br>
2026-04-15 17:29:18 -0300

    feat: Add OrderRepository Interface

---
+++ Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task AddAsync(Order order);
+        Task<Order?> GetByIdAsync(Guid id);
+        Task<List<Order>> GetAllAsync();
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(Guid id);
+    }
+}
