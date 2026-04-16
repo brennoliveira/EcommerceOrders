75e62bc6aae96b94016030d5a22e257921dd3d74
Brenno <brenno.oliveira@souunit.com.br>
2026-04-15 22:42:18 -0300

    feat: Add CreateOrder controller

---
+++ Api/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Application.UseCases.Orders.CreateOrder;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/orders")]
+    public class OrdersController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateOrderCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id }, null);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok(); // TODO
+        }
+    }
+}
